d9463209adb7d9efee71dd8b5c54528b593d7c92
princelonappan07 <princel@qburst.com>
2020-11-28 12:00:24 +0530

    first commit

---
+++ core/settings.php
@@ -0,0 +1,23 @@
+<?php
+# @*************************************************************************@
+# @ @author Mansur Altamirov (Mansur_TL)									@
+# @ @author_url 1: https://www.instagram.com/mansur_tl                      @
+# @ @author_url 2: http://codecanyon.net/user/mansur_tl                     @
+# @ @author_email: highexpresstore@gmail.com                                @
+# @*************************************************************************@
+# @ ColibriSM - The Ultimate Modern Social Media Sharing Platform           @
+# @ Copyright (c) 21.03.2020 ColibriSM. All rights reserved.                @
+# @*************************************************************************@
+
+
+# MySQL db host name
+$sql_db_host = "127.0.0.1";
+# MySQL db User
+$sql_db_user = "root";
+# MySQL db Password
+$sql_db_pass = "root";
+# MySQL db Name
+$sql_db_name = "yusuf";
+# Site URL
+$site_url    = "http://localhost:83/"; // E.g (http://example.com)
+?>
